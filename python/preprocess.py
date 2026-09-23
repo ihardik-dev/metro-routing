d18a25151aea76e3461b23ea364d81092a1d2350
Hardik <hardikmakkar.dev@gmail.com>
2026-09-23 23:43:28 +0530

    added load_stop_times

--- python/preprocess.py
+++ python/preprocess.py
@@ -3,6 +3,7 @@ import csv
 STOPS_FILE = "data/raw/gtfs/stops.txt"
 ROUTES_FILE = "data/raw/gtfs/routes.txt"
 TRIPS_FILE = "data/raw/gtfs/trips.txt"
+STOP_TIMES_FILE = "data/raw/gtfs/stop_times.txt"
 
 def load_stops():
     stops ={}
@@ -38,6 +39,7 @@ def load_routes():
 
     return routes
 
+
 def load_trips():
     trips = {}
 
@@ -53,13 +55,39 @@ def load_trips():
 
     return trips
 
+
+def load_stop_times():
+    stop_times = {}
+
+    with open(STOP_TIMES_FILE, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            trip_id = int(row["trip_id"])
+
+            if trip_id not in stop_times:
+                stop_times[trip_id] = []
+
+            stop_times[trip_id].append({
+                "stop_id": int(row["stop_id"]),
+                "sequence": int(row["stop_sequence"]),
+                "arrival": row["arrival_time"],
+                "departure": row["departure_time"],
+            })
+
+    return stop_times    
+
 if __name__ == "__main__":
     stops = load_stops()
     routes = load_routes()
     trips = load_trips()
+    stop_times = load_stop_times()
 
-    print(f"Total stations: {len(stops)}")
-    print(f"Total routes: {len(routes)}")
-    print(f"Total trips: {len(trips)}")
+    trip = trips[0]
+    route = routes[trip["route_id"]]
 
-    print(trips[0])
+    print("Trip:", 0)
+    print("Route:", route["short_name"])
+    print("Service:", route["long_name"])
+
+    print(stop_times[0])
